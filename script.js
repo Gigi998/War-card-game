da568cf298ce569ac40d9f1eccfd8d1ca51c7989
Luigi <luigi.drnasin12@gmail.com>
2022-03-09 11:40:39 +0100

    style.css

---
+++ script.js
@@ -0,0 +1,16 @@
+let card1 = 5;
+let card2 = 6;
+
+let card1El = document.getElementById("card1");
+let card2El = document.getElementById("card2");
+
+
+function newGame(){
+    card1El.textContent = "Player1 card: " + card1;
+    card2El.textContent = "Player2 card: " + card2;
+    }
+
+
+function newRound(){
+    return
+}
